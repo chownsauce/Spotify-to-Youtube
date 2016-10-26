9c54538cca78206ea711c4bee15698a9b1c13aa0
CTSD <clement.trestard@gmail.com>
2016-10-26 13:58:08 +0200

    Logging on Spotify and fetching data

---
+++ config.js
@@ -0,0 +1,8 @@
+var config = {
+
+  spotify: {
+    clientId: "b92710d7bc9846309e7441a0c1e153fe",
+    redirectUri: "http://localhost:8888/GitHub/Spotify-to-Youtube/"
+  }
+
+};
--- main.js
+++ main.js
@@ -1,10 +1,60 @@
-var app = angular.module('app', ['appControllers','appServices','appFilters']);
+var app = angular
+  .module('app', ['appControllers','appServices','appFilters', 'spotify'])
+  .config(function (SpotifyProvider) {
+    SpotifyProvider.setClientId(config.spotify.clientId);
+    SpotifyProvider.setRedirectUri(config.spotify.redirectUri);
+    SpotifyProvider.setScope('playlist-read-private playlist-read-collaborative');
+    if (localStorage.getItem('spotify-token'))
+      SpotifyProvider.setAuthToken(localStorage.getItem('spotify-token'));
+  });
 var appControllers = angular.module('appControllers', []);
 var appServices = angular.module('appServices', []);
 var appFilters = angular.module('appFilters', []);
 
-appControllers.controller('MainController', [ '$scope', function ($scope) {
+appControllers.controller('MainController', [ '$scope', 'Spotify', function ($scope, Spotify) {
 
-  $scope.message = "Hello world !";
+  // Getting Spotify token from URL
+  var hash = window.location.hash;
+  if (window.location.search.substring(1).indexOf("error") !== -1) {}
+  else if (hash) {
+    var token = window.location.hash.split('&')[0].split('=')[1];
+    localStorage.setItem('spotify-token', token);
+  }
+
+  // Getting Spotify token from local storage
+  $scope.spotifyToken = localStorage.getItem('spotify-token');
+
+  if ($scope.spotifyToken)
+    loadSpotifyData();
+
+  $scope.spotifyLogin = function () {
+    Spotify.login()
+      .then(function (data) {
+        loadSpotifyData();
+      });
+  };
+
+  $scope.spotifyUser = null;
+  $scope.spotifyPlaylists = null;
+  $scope.selectedPlaylist = { playlist: "" };
+  function loadSpotifyData() {
+    Spotify.getCurrentUser().then(function(data) {
+      $scope.spotifyUser = data;
+      loadSpotifyPlaylists();
+    });
+  }
+
+  function loadSpotifyPlaylists() {
+    Spotify.getUserPlaylists($scope.spotifyUser.id, { limit: 50 }).then(function(data) {
+      $scope.spotifyPlaylists = data;
+    });
+  }
+
+  $scope.convert = function() {
+    if ($scope.selectedPlaylist.playlist != "")
+      console.log("Converting Spotify playlist " + $scope.selectedPlaylist.playlist.id);
+  };
+
+
 
 }]);
