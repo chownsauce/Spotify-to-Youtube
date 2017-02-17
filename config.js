31ff0187e296c5fed98f1b40c4689a2cdd195fbc
CTSD <clement.trestard@gmail.com>
2017-02-17 11:44:28 +0100

    Updating config for prod

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ var config = {
 
   spotify: {
     clientId: "b92710d7bc9846309e7441a0c1e153fe",
-    redirectUri: "http://localhost:8888/GitHub/Spotify-to-Youtube/"
+    redirectUri: "http://ctsd.github.io/Spotify-to-Youtube/"
   },
 
   youtube: {
